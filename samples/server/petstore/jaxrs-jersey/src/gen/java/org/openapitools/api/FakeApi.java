ffdd5f813325c08be2c7ba911fe599be76bfef9a
sullis <github@seansullivan.com>
2022-03-16 22:25:40 -0700

    regenerate samples and docs

--- samples/server/petstore/jaxrs-jersey/src/gen/java/org/openapitools/api/FakeApi.java
+++ samples/server/petstore/jaxrs-jersey/src/gen/java/org/openapitools/api/FakeApi.java
@@ -30,10 +30,10 @@ import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 
 import javax.servlet.ServletConfig;
-import java.ws.rs.core.Context;
-import java.ws.rs.core.Response;
-import java.ws.rs.core.SecurityContext;
-import java.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.*;
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 
@@ -66,7 +66,7 @@ public class FakeApi  {
       this.delegate = delegate;
    }
 
-    @java.ws.rs.GET
+    @javax.ws.rs.GET
     @Path("/health")
     
     @Produces({ "application/json" })
@@ -78,7 +78,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakeHealthGet(securityContext);
     }
-    @java.ws.rs.GET
+    @javax.ws.rs.GET
     @Path("/http-signature-test")
     @Consumes({ "application/json", "application/xml" })
     
@@ -92,7 +92,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakeHttpSignatureTest(pet, query1, header1, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/outer/boolean")
     @Consumes({ "application/json" })
     @Produces({ "*/*" })
@@ -104,7 +104,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakeOuterBooleanSerialize(body, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/outer/composite")
     @Consumes({ "application/json" })
     @Produces({ "*/*" })
@@ -116,7 +116,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakeOuterCompositeSerialize(outerComposite, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/outer/number")
     @Consumes({ "application/json" })
     @Produces({ "*/*" })
@@ -128,7 +128,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakeOuterNumberSerialize(body, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/outer/string")
     @Consumes({ "application/json" })
     @Produces({ "*/*" })
@@ -140,7 +140,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakeOuterStringSerialize(body, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/property/enum-int")
     @Consumes({ "application/json" })
     @Produces({ "*/*" })
@@ -152,7 +152,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.fakePropertyEnumIntegerSerialize(outerObjectWithEnumProperty, securityContext);
     }
-    @java.ws.rs.PUT
+    @javax.ws.rs.PUT
     @Path("/body-with-binary")
     @Consumes({ "image/png" })
     
@@ -164,7 +164,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testBodyWithBinary(body, securityContext);
     }
-    @java.ws.rs.PUT
+    @javax.ws.rs.PUT
     @Path("/body-with-file-schema")
     @Consumes({ "application/json" })
     
@@ -176,7 +176,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testBodyWithFileSchema(fileSchemaTestClass, securityContext);
     }
-    @java.ws.rs.PUT
+    @javax.ws.rs.PUT
     @Path("/body-with-query-params")
     @Consumes({ "application/json" })
     
@@ -188,7 +188,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testBodyWithQueryParams(query, user, securityContext);
     }
-    @java.ws.rs.PATCH
+    @javax.ws.rs.PATCH
     
     @Consumes({ "application/json" })
     @Produces({ "application/json" })
@@ -200,7 +200,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testClientModel(client, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     
     @Consumes({ "application/x-www-form-urlencoded" })
     
@@ -216,7 +216,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testEndpointParameters(number, _double, patternWithoutDelimiter, _byte, integer, int32, int64, _float, string, binaryBodypart, date, dateTime, password, paramCallback, securityContext);
     }
-    @java.ws.rs.GET
+    @javax.ws.rs.GET
     
     @Consumes({ "application/x-www-form-urlencoded" })
     
@@ -229,7 +229,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testEnumParameters(enumHeaderStringArray, enumHeaderString, enumQueryStringArray, enumQueryString, enumQueryInteger, enumQueryDouble, enumFormStringArray, enumFormString, securityContext);
     }
-    @java.ws.rs.DELETE
+    @javax.ws.rs.DELETE
     
     
     
@@ -243,7 +243,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testGroupParameters(requiredStringGroup, requiredBooleanGroup, requiredInt64Group, stringGroup, booleanGroup, int64Group, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/inline-additionalProperties")
     @Consumes({ "application/json" })
     
@@ -255,7 +255,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testInlineAdditionalProperties(requestBody, securityContext);
     }
-    @java.ws.rs.GET
+    @javax.ws.rs.GET
     @Path("/jsonFormData")
     @Consumes({ "application/x-www-form-urlencoded" })
     
@@ -267,7 +267,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testJsonFormData(param, param2, securityContext);
     }
-    @java.ws.rs.PUT
+    @javax.ws.rs.PUT
     @Path("/test-query-parameters")
     
     
@@ -279,7 +279,7 @@ public class FakeApi  {
     throws NotFoundException {
         return delegate.testQueryParameterCollectionFormat(pipe, ioutil, http, url, context, allowEmpty, language, securityContext);
     }
-    @java.ws.rs.POST
+    @javax.ws.rs.POST
     @Path("/{petId}/uploadImageWithRequiredFile")
     @Consumes({ "multipart/form-data" })
     @Produces({ "application/json" })
